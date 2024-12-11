b45b513a807b5350147c37b0a92ef7aa7bc6c646
Niaz Bin Siraj <niaz9767@gmail.com>
2024-12-12 01:36:00 +0600

    Created spring boot project

---
+++ src/main/java/com/turingsouls/book_crud_service/BookCrudServiceApplication.java
@@ -0,0 +1,13 @@
+package com.turingsouls.book_crud_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookCrudServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookCrudServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/turingsouls/book_crud_service/BookCrudServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turingsouls.book_crud_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookCrudServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
